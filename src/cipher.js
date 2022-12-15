c9c3651765bd370eac8cdabd73a13c72ade54f9d
YesseniaEspinosa8 <yeyeespinosa8@gmail.com>
2022-12-15 15:37:53 -0600

    Agruegue metodo encode y descode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,49 @@
 const cipher = {
-  // ...
+  encode: function (offset, string) {
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError();
+    }
+    let textEncode = "";
+    for (let index = 0; index < string.length; index++) {
+      let codeChar = string.charCodeAt(index);
+      let charCipher;
+      if (codeChar < -65 || codeChar > 90) {
+        charCipher = String.fromChardCode(codeChar);
+        textEncode += charCipher;
+
+      } else {
+        let codeCharEncode = (codeChar - 65 + offset) % 26 + 65;
+        charCipher = String.fromCharCode(codeCharEncode);
+        textEncode = textEncode + charCipher;
+      }
+    }
+    return textEncode;
+  },
+
+
+  decode: function (offset, string) {
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError();
+    }
+    let textDecode = "";
+    for (let index = 0; index < string.length; index++) {
+      let codeChar = string.charCodeAt(index);
+      let charCipher;
+      if (codeChar < 65 || codeChar > 90) {
+        charCipher = String.fromCharCode(codeChar);
+        textDecode += charCipher;
+
+      } else {
+        let codeCharDecode = (codeChar - 90 - offset) % 26 + 90;
+        charCipher = String.fromCharCode(codeCharDecode);
+        textDecode = textDecode + charCipher;
+
+      }
+    }
+    return textDecode;
+  }
+
 };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
+// Obtener el elemento imput (clave)
+let key = document.getElementById("key");
+// Obtener el elemento textarea
+let textUser = document.getElementById("TextToEncrypt");
+// Agregar escuchador al textarea
+textUser.addEventListener("keydown", checkChar);
+// Obetner el elemento p (resultado)
+let containerTxtResult = document.getElementById("result");
+
+// Obtener el elemento button ENCRYPT
+let btnEncrypt= document.getElementById ("btnEncrypt");
+
 
 console.log(cipher);
